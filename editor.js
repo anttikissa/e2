f33950287c0f61e5a881af8de7b5d4c0dd205580
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2020-08-07 18:32:25 +0300

    Fix indentation

--- editor.js
+++ editor.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 
 let state = {
-  currentFile: null
+	currentFile: null
 }
 
 try {
-  state = JSON.parse(fs.readFileSync('.e-state', 'utf8'))
+	state = JSON.parse(fs.readFileSync('.e-state', 'utf8'))
 } catch (e) {
 }
 
@@ -14,33 +14,33 @@ window.addEventListener('beforeunload', () => {
 })
 
 class SimpleEditor {
-  constructor(globalState) {
-    this.globalState = globalState
-    this.filename = null
-    this.ta = document.createElement('textarea')
-    this.ta.setAttribute('spellcheck', 'false')
-    this.ta.addEventListener('keydown', e => {
-	if (e.metaKey && e.key === 's') {
-		this.save()
-		alert('file ' + this.filename + ' saved!')
+	constructor(globalState) {
+		this.globalState = globalState
+		this.filename = null
+		this.ta = document.createElement('textarea')
+		this.ta.setAttribute('spellcheck', 'false')
+		this.ta.addEventListener('keydown', e => {
+			if (e.metaKey && e.key === 's') {
+				this.save()
+				alert('file ' + this.filename + ' saved!')
+			}
+		})
 	}
-    })
-  }
 
-  open(name) {
+	open(name) {
 	this.filename = name
-    this.globalState.currentFile = name
-    try {
-      let file = fs.readFileSync(this.filename, 'utf8')
-      this.ta.value = file.replace(/( )( )/g, '\t')
-    } catch (e) {
-  this.ta.value = ''
-    }
-  }
+		this.globalState.currentFile = name
+		try {
+			let file = fs.readFileSync(this.filename, 'utf8')
+			this.ta.value = file.replace(/( )( )/g, '\t')
+		} catch (e) {
+	    this.ta.value = ''
+		}
+	}
 
-  save() {
-	fs.writeFileSync(this.filename, this.ta.value, 'utf8')
-  }
+	save() {
+  	fs.writeFileSync(this.filename, this.ta.value, 'utf8')
+	}
 }
 
 let editor = new SimpleEditor(state)
@@ -48,11 +48,11 @@ let editor = new SimpleEditor(state)
 document.body.appendChild(editor.ta)
 
 if (state.currentFile) {
-  editor.open(state.currentFile)
+	editor.open(state.currentFile)
 }
 
 function o(name) {
-  editor.open(name)
+	editor.open(name)
 }
 
 

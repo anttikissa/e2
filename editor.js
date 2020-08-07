7b0eeae72181f26be2e8edeb29cfa738d8fa4f93
Antti Kissaniemi <antti.kissaniemi@gmail.com>
2020-08-07 20:00:49 +0300

    Simple editor: implement inserting tab

--- editor.js
+++ editor.js
@@ -1,5 +1,7 @@
 let fs = require('fs')
 
+let { log } = console
+
 let state = {
 	currentFile: null
 }
@@ -19,27 +21,39 @@ class SimpleEditor {
 		this.filename = null
 		this.ta = document.createElement('textarea')
 		this.ta.setAttribute('spellcheck', 'false')
+
 		this.ta.addEventListener('keydown', e => {
+			log('keydown', e.key)
+
 			if (e.metaKey && e.key === 's') {
 				this.save()
 				alert('file ' + this.filename + ' saved!')
+			}
+
+			if (e.key === 'Tab') {
+				e.preventDefault()
+				let before = this.ta.value.substring(0, this.ta.selectionStart)
+				let endPos = this.ta.selectionEnd
+				let after = this.ta.value.substring(this.ta.selectionEnd)
+				this.ta.value = before + '\t' + after
+				this.ta.selectionStart = this.ta.selectionEnd = endPos + 1
 			}
 		})
 	}
 
 	open(name) {
-	this.filename = name
+		this.filename = name
 		this.globalState.currentFile = name
 		try {
 			let file = fs.readFileSync(this.filename, 'utf8')
 			this.ta.value = file.replace(/( )( )/g, '\t')
 		} catch (e) {
-	    this.ta.value = ''
+			this.ta.value = ''
 		}
 	}
 
 	save() {
-  	fs.writeFileSync(this.filename, this.ta.value, 'utf8')
+		fs.writeFileSync(this.filename, this.ta.value, 'utf8')
 	}
 }
 
@@ -56,3 +70,4 @@ function o(name) {
 }
 
 
+		
